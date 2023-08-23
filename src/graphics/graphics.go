5aa3a2d2e142a3ed96eb6f206cfb39e4981238c1
jonathan <jonathan.willian321@gmail.com>
2023-08-23 18:22:35 -0300

    refatorando

--- src/graphics/graphics.go
+++ src/graphics/graphics.go
@@ -14,22 +14,41 @@ const (
 	WITDH, HEIGTH = 800, 600
 )
 
-// Enumeration
+type (
+	SDLWindow struct {
+		window    *sdl.Window
+		renderer  *sdl.Renderer
+		font      *ttf.Font
+		terminate bool
+		fps       int
+	}
 
-var (
-	window       *sdl.Window
-	renderer     *sdl.Renderer
-	font         *ttf.Font
-	states       map[string]*graphicalState
-	currentState string
-	LastMessage  = machine.STATE_NOT_CHANGE
+	drag struct {
+		clickOffset   *sdl.Point
+		selected      *objectInfo
+		leftMouseDown bool
+	}
+
+	objectInfo struct {
+		object any
+		*sdl.Point
+	}
+
+	machineChannel struct {
+		channel   chan int
+		lastMsg   int
+		executing bool
+	}
 )
 
-type SDLWindow struct {
-	window   *sdl.Window
-	renderer *sdl.Renderer
-	font     *ttf.Font
-}
+var (
+	states        map[string]*graphicalState
+	currentState  string
+	LastMessage   = machine.STATE_NOT_CHANGE
+	mousePos      *sdl.Point
+	dragInfo      *drag
+	communication *machineChannel
+)
 
 func NewSDLWindow() *SDLWindow {
 	err := sdl.Init(sdl.INIT_EVERYTHING)
@@ -38,13 +57,13 @@ func NewSDLWindow() *SDLWindow {
 	}
 
 	// TODO: title of the window
-	window, err = sdl.CreateWindow("Autômato Simulator", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+	window, err := sdl.CreateWindow("Simulador de Autômato", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
 		WITDH, HEIGTH, sdl.WINDOW_SHOWN)
 	if err != nil {
 		panic(err)
 	}
 
-	renderer, err = sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
+	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
 	if err != nil {
 		panic(err)
 	}
@@ -54,7 +73,7 @@ func NewSDLWindow() *SDLWindow {
 		panic(err)
 	}
 
-	font, err = ttf.OpenFont("/home/jonathan/programacao/autosimulator/src/graphics/assets/IBMPlexMono-ExtraLight.ttf", 12)
+	font, err := ttf.OpenFont("/home/jonathan/programacao/autosimulator/src/graphics/assets/IBMPlexMono-ExtraLight.ttf", 12)
 	if err != nil {
 		panic(err)
 	}
@@ -64,10 +83,98 @@ func NewSDLWindow() *SDLWindow {
 	return &SDLWindow{window: window, renderer: renderer, font: font}
 }
 
+func init() {
+	dragInfo = &drag{clickOffset: &sdl.Point{X: 0, Y: 0}, selected: nil, leftMouseDown: false}
+	communication = &machineChannel{channel: make(chan int), lastMsg: machine.STATE_NOT_CHANGE, executing: false}
+}
+
+func (w *SDLWindow) Destroy() {
+	sdl.Quit()
+	ttf.Quit()
+	w.window.Destroy()
+	w.renderer.Destroy()
+	w.font.Close()
+}
+
 func (w *SDLWindow) pollEvent() {
+	event := sdl.PollEvent()
+	for event != nil {
+		switch event.(type) {
+		case *sdl.QuitEvent:
+			w.Destroy()
+			w.terminate = true
+
+		case *sdl.KeyboardEvent:
+			handleKeyboardEvents(event.(*sdl.KeyboardEvent), w)
+
+		case *sdl.MouseButtonEvent:
+			handleMouseButtonEvents(event.(*sdl.MouseButtonEvent))
+
+		case *sdl.MouseMotionEvent:
+			handleMouseMotionEvent()
+
+		default:
+		}
+
+		event = sdl.PollEvent()
+	}
+}
+
+func handleKeyboardEvents(event *sdl.KeyboardEvent, w *SDLWindow) {
+	if event.Keysym.Sym == sdl.K_RETURN &&
+		!communication.executing {
+		fmt.Println("executing")
+		w.fps = 1
+		communication.executing = true
+		// Machine?
+		go machine.Execute(m, []string{"a", "a", "b"}, communication.channel)
+	}
 
 }
 
+func handleMouseButtonEvents(event *sdl.MouseButtonEvent) {
+	if event.Button != sdl.BUTTON_LEFT {
+		// Nothing to do with other buttons
+		return
+	}
+
+	// IF THE LEFT MOUSE BUTTON IS PRESSED
+	switch event.Type {
+	case sdl.MOUSEBUTTONDOWN:
+		if !dragInfo.leftMouseDown {
+			dragInfo.leftMouseDown = true
+			for _, state := range states {
+				if mousePos.InRect(state.Rect) {
+					object := &objectInfo{object: state, Point: &sdl.Point{X: state.X, Y: state.Y}}
+					dragInfo.selected = object
+					dragInfo.clickOffset.X = mousePos.X - state.X
+					dragInfo.clickOffset.Y = mousePos.Y - state.Y
+					break
+				}
+			}
+		}
+
+	case sdl.MOUSEBUTTONUP:
+		if dragInfo.leftMouseDown {
+			dragInfo.leftMouseDown = false
+			dragInfo.selected = nil
+		}
+	}
+}
+
+func handleMouseMotionEvent() {
+	mousePos.X, mousePos.Y, _ = sdl.GetMouseState()
+	if dragInfo.leftMouseDown && dragInfo.selected != nil {
+		dragObject()
+	}
+}
+
+func dragObject() {
+	dragInfo.selected.X = mousePos.X - dragInfo.clickOffset.X
+	dragInfo.selected.Y = mousePos.Y - dragInfo.clickOffset.Y
+}
+
+// ///////////////////////////////////////////
 func Run(m machine.Machine) {
 	channel := make(chan int)
 	var fps uint32 = 1000 / 60
@@ -173,19 +280,6 @@ func Run(m machine.Machine) {
 
 	defer shutDown()
 }
-
-// Inits / shutdows
-
-func shutDown() {
-	sdl.Quit()
-	ttf.Quit()
-	window.Destroy()
-	renderer.Destroy()
-	font.Close()
-}
-
-/////////////////////////////////////////////
-
 func drawExs() {
 	gfx.ThickLineColor(renderer, 0, HEIGTH/2, WITDH, HEIGTH/2, 1, BLACK)
 	gfx.ThickLineColor(renderer, WITDH/2, 0, WITDH/2, HEIGTH, 1, BLACK)
